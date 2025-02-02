26bac654ea6c457c9cd5266ddf932744f4c8cf04
eu-helio <helio2rn1@gmail.com>
2025-02-02 20:57:50 -0300

    ver1

--- Automatizador bkp git/index.py
+++ Automatizador bkp git/index.py
@@ -1,2 +1,46 @@
-user = ""
-pw = ""
+from datetime import datetime
+import sys
+import subprocess
+import os
+ultima_data = "2025-02-02"
+
+# Obtém a data e hora atual ( testado ok)
+def verificar_dia_atual():
+    data = datetime.now()
+    # Formata a data atual no formato desejado (ex: YYYY-MM-DD)
+    data_atual = data.strftime("%d-%m-%Y")
+    return data_atual
+    
+def git_push(folder_path, commit_message):
+    try:
+        # Mudar para o diretório da pasta especificada
+        os.chdir(folder_path)
+
+        # Adicionar todas as alterações ao repositório Git
+        subprocess.run(["git", "add", "."], check=True)
+
+        # Fazer commit das alterações com a mensagem especificada
+        subprocess.run(["git", "commit", "-m", commit_message], check=True)
+
+        # Fazer push das alterações para o repositório remoto
+        subprocess.run(["git", "push"], check=True)
+
+        print("Upload para o Git concluído com sucesso!")
+
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Uso: python script.py <caminho_da_pasta> <mensagem_de_commit>")
+    else:
+        folder_path = sys.argv[1]
+        commit_message = sys.argv[2]
+        git_push(folder_path, commit_message)
+
+# Comparar data atual com o ultimo check
+def verificar_ultimocheck():
+    if verificar_dia_atual > ultima_data:
+        print("OI")  #codigo bkp
+    elif verificar_dia_atual == ultima_data:
+        print("OI") #verificar se vai haver atualizações no dia, estimar tempo para bkp
