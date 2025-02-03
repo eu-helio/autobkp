a83cd95cefaced89fece24ff48d100fec81fae21
eu-helio <helio2rn1@gmail.com>
2025-02-02 22:45:45 -0300

    ver1.1

--- Automatizador bkp git/index.py
+++ Automatizador bkp git/index.py
@@ -2,15 +2,22 @@ from datetime import datetime
 import sys
 import subprocess
 import os
-ultima_data = "2025-02-02"
+folder_path = "\Projetos\Projeto teste"
+ultima_data = "01-02-2025"
 
 # Obtém a data e hora atual ( testado ok)
 def verificar_dia_atual():
     data = datetime.now()
     # Formata a data atual no formato desejado (ex: YYYY-MM-DD)
-    data_atual = data.strftime("%d-%m-%Y")
+    data_atual = data.strftime("%d-%m-%Y")    
     return data_atual
     
+verificar_dia_atual()
+commit_message = (verificar_dia_atual())
+data_actual = verificar_dia_atual()
+
+    
+
 def git_push(folder_path, commit_message):
     try:
         # Mudar para o diretório da pasta especificada
@@ -30,17 +37,25 @@ def git_push(folder_path, commit_message):
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
 
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Uso: python script.py <caminho_da_pasta> <mensagem_de_commit>")
-    else:
-        folder_path = sys.argv[1]
-        commit_message = sys.argv[2]
-        git_push(folder_path, commit_message)
+
+
+
 
 # Comparar data atual com o ultimo check
 def verificar_ultimocheck():
-    if verificar_dia_atual > ultima_data:
-        print("OI")  #codigo bkp
-    elif verificar_dia_atual == ultima_data:
+    if data_actual > ultima_data:
+        git_push
+    elif data_actual == ultima_data:
         print("OI") #verificar se vai haver atualizações no dia, estimar tempo para bkp
+        
+verificar_ultimocheck()
+
+
+def exec():
+    if __name__ == "__main__":
+        if len(sys.argv) != 3:
+            print("Uso: python script.py <caminho_da_pasta> <mensagem_de_commit>")
+        else:
+            folder_path = sys.argv[1]
+            commit_message = sys.argv[2]
+            git_push(folder_path, commit_message)
